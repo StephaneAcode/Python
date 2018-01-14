472b9f40dd5e43727d691aeda4b87094239c7da7
Stéphane Andrieux <stephane@macbook-air-de-stephane.home>
2018-01-14 22:17:08 +0100

    Implemented Setitem and Getitem

--- dictOrdo.py
+++ dictOrdo.py
@@ -2,3 +2,53 @@
 # coding: utf-8
 
 
+
+
+class DictionnaireOrdonne():
+    
+    
+    def __init__(self):
+        self._dicKeys = []
+        self._dicValues = []
+        print "Constructeur"
+        
+
+
+    #Pour gerer:
+    #objet[index];
+    #objet[index] = valeur;
+    #del objet[index];
+    
+    def __getitem__(self, reqKey):
+        
+        if reqKey in self._dicKeys:
+            dicIndex = self._dicKeys.index(reqKey)
+            print "Getting Key %s ( %s ) at index %s" % (reqKey, self._dicValues[dicIndex], dicIndex)
+            return self._dicValues[dicIndex]
+
+    def __setitem__(self, reqKey, reqValue):
+        
+        if reqKey in self._dicKeys:
+            dicIndex = self._dicKeys.index(reqKey)
+            self._dicValues[dicIndex] = reqValue
+            print "Setting Key %s = %s to index %s" % (reqKey, reqValue, dicIndex)
+        else:
+            print "Adding Key %s = %s to index %s" % (reqKey, reqValue, len(self._dicKeys))
+            self._dicKeys.append(reqKey)
+            self._dicValues.append(reqValue)
+            print self._dicKeys
+
+
+    #def __delitem__():
+
+
+print "Coucou"
+
+myD = DictionnaireOrdonne()
+myD["A"] = "a"
+myD["B"] = "b"
+myD["C"] = "c"
+
+myD["C"]
+myD["B"]
+myD["A"]
