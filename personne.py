d752641899959005a12d92b0e8ceabeab4da62b7
Stéphane Andrieux <stephane@macbook-air-de-stephane.home>
2018-01-14 11:09:51 +0100

    Updates from the local Mac files

--- personne.py
+++ personne.py
@@ -72,9 +72,53 @@ class Test:
     afficher = staticmethod(afficher)
 
 
+
+class Personne:
+    """Classe définissant une personne caractérisée par :
+    - son nom ;
+    - son prénom ;
+    - son âge ;
+    - son lieu de résidence"""
+
+    
+    def __init__(self, nom, prenom):
+        """Constructeur de notre classe"""
+        self.nom = nom
+        self.prenom = prenom
+        self.age = 33
+        self._lieu_residence = "Paris" # Notez le souligné _ devant le nom
+    def _get_lieu_residence(self):
+    """Méthode qui sera appelée quand on souhaitera accéder en lecture
+        à l'attribut 'lieu_residence'"""
+        
+        
+        print("On accède à l'attribut lieu_residence !")
+        return self._lieu_residence
+    def _set_lieu_residence(self, nouvelle_residence):
+        """Méthode appelée quand on souhaite modifier le lieu de résidence"""
+        print("Attention, il semble que {} déménage à {}.".format( \
+                self.prenom, nouvelle_residence))
+        self._lieu_residence = nouvelle_residence
+    # On va dire à Python que notre attribut lieu_residence pointe vers une
+    # propriété
+    lieu_residence = property(_get_lieu_residence, _set_lieu_residence)
+    
+
+
 sys.exit()
 
 print "Coucou\n"
+
+jean = Personne()
+david = Personne()
+print jean.nom
+print david.nom
+jean.prenom
+jean.age
+jean.lieu_residence
+# Jean demenage...
+jean.lieu_residence = "Berlin"
+jean.lieu_residence
 
 a = Compteur()
 b = Compteur()
@@ -95,7 +139,14 @@ tab.lire()
 
 
 
+Compteur.combien()
+a = Compteur()
+Compteur.combien()
+b = Compteur()
+Compteur.combien()
 
+pan = Test()
+pan.afficher()
 
 
 
